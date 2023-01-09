ec6a3d2dcd99c20ddac67d80726c695dc144047b
Anushka Kamalja <anushkakamalja@gmail.com>
2023-01-09 16:23:22 +0530

    Filtering according to multiple unfamiliar skills

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ today = date.today()
 # print(today)
 
 print("Enter some skills that you are unfamiliar with")
-unfamiliar_skills= input(">")
+unfamiliar_skills= list(input(">").split(","))
 print(f'Filtering out {unfamiliar_skills}')
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
@@ -16,12 +16,17 @@ for job in jobs:
     if published_date[7].isnumeric() :
         date = today - timedelta(days= int(published_date[7]))
         comp_name = job.find('h3', class_ = 'joblist-comp-name').text.strip()
-        skills = job.find('span', class_ = 'srp-skills').text.replace(" ", "")
+        skills = job.find('span', class_ = 'srp-skills').text
         # print(skills)
         more_info = job.header.h2.a['href']
-        if unfamiliar_skills not in skills:
+        unfamiliar_skill_detected = False
+        for unfamiliar_skill in unfamiliar_skills:
+            if unfamiliar_skill.lower() in skills.lower():
+                unfamiliar_skill_detected = True
+             
+        if unfamiliar_skill_detected is False:
             print(f"Company Name: {comp_name}")
             print(f"Required Skills: {skills.strip()}")
             print(f"Published Date: {date}")
             print(f"More Info: {more_info}")
-            print('')
+            print('')    
